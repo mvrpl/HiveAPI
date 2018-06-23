408928f91cf65c5f5989cf11dabe1a784a7d27c6
Marcos Vinícius Rodrigues <mvro.dev@gmail.com>
2018-06-23 18:47:03 -0300

    Adicionado função de executar query em background com java thread

--- src/main/java/com/app/Hive.java
+++ src/main/java/com/app/Hive.java
@@ -17,12 +17,15 @@ import com.cachelib.QueryKey;
 import com.cachelib.QueryResult;
 import com.dados.JsonObj;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Hive {
+    private final String driverName;
+    private JCS cache;
 
-    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
-
-    public String jsonData(final String userQuery) throws SQLException, ClassNotFoundException, CacheException {
-        Connection con = null;
+    public Hive() {
+        this.driverName = "org.apache.hive.jdbc.HiveDriver";
 
         CompositeCacheManager ccm = CompositeCacheManager.getUnconfiguredInstance();
         Properties props = new Properties();
@@ -37,48 +40,69 @@ public class Hive {
         props.put("jcs.auxiliary.DC.attributes.diskPath", "/Users/marcos/JavaHive/HiveAPI/cache");
         props.put("jcs.auxiliary.DC.attributes.MaxPurgatorySize", "10000");
         props.put("jcs.auxiliary.DC.attributes.MaxKeySize", "10000");
+        props.put("jcs.auxiliary.DC.attributes.MaxLife", "10");
         props.put("jcs.auxiliary.DC.attributes.OptimizeAtRemoveCount", "300000");
         props.put("jcs.auxiliary.DC.attributes.OptimizeOnShutdown", "true");
         props.put("jcs.auxiliary.DC.attributes.DiskLimitType", "COUNT");
-        
         ccm.configure(props);
 
-        JCS cache = JCS.getInstance("default");
+        try {
+            this.cache = JCS.getInstance("default");
+        } catch (CacheException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public String jsonData(final String userQuery) throws ClassNotFoundException, SQLException, CacheException {
 
         QueryKey queryKey = new QueryKey(userQuery);
 
         QueryResult resultSet = (QueryResult) cache.get(queryKey.getQueryText());
 
         if (resultSet == null) {
-
             Class.forName(driverName);
 
-            con = DriverManager.getConnection("jdbc:hive2://127.0.0.1:10000/default", "hive", "");
+            JCS cacheSys = this.cache;
 
-            Statement stmt = con.createStatement();
+            List tempRows = new ArrayList();
+            HashMap<String, String> tempRow = new HashMap<String, String>();
+            tempRow.put("STATUS", "running");
+            tempRows.add(tempRow);
+            resultSet = new QueryResult(0, tempRows);
+            cacheSys.put(queryKey.getQueryText(), resultSet);
 
-            final List rows = new ArrayList();
-
-            ResultSet resultSQL = stmt.executeQuery(userQuery);
-
-            ResultSetMetaData rsmd = resultSQL.getMetaData();
-
-            final int columnCount = resultSQL.getMetaData().getColumnCount();
-
-            while (resultSQL.next()) {
-                HashMap<String, String> row = new HashMap<String, String>();
-                for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                    row.put(rsmd.getColumnName(columnIndex), resultSQL.getString(columnIndex));
+            Runnable r = new Runnable() {
+                public void run() {
+                    try {
+                        Connection con = DriverManager.getConnection("jdbc:hive2://127.0.0.1:10000/default", "hive", "");
+                        Statement stmt = con.createStatement();
+                        final List rows = new ArrayList();
+                        ResultSet resultSQL = stmt.executeQuery(userQuery);
+                        ResultSetMetaData rsmd = resultSQL.getMetaData();
+                        final int columnCount = resultSQL.getMetaData().getColumnCount();
+                        while (resultSQL.next()) {
+                            HashMap<String, String> row = new HashMap<String, String>();
+                            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                                row.put(rsmd.getColumnName(columnIndex), resultSQL.getString(columnIndex));
+                            }
+                            rows.add(row);
+                        }
+                        QueryResult hiveResultSet = new QueryResult(columnCount, rows);
+                        cacheSys.put(queryKey.getQueryText(), hiveResultSet);
+                        con.close();
+                    } catch (SQLException|CacheException e) {
+                        System.err.println(e.getMessage());
+                        try {
+                            cacheSys.remove( queryKey.getQueryText() );
+                        } catch (CacheException err) {
+                            System.err.println(err.getMessage());
+                        }
+                    }
                 }
-                rows.add(row);
-            }
-
-            resultSet = new QueryResult(columnCount, rows);
-            cache.put(queryKey.getQueryText(), resultSet);
-        }
-
-        if (con != null) {
-            con.close();
+            };
+            ExecutorService executor = Executors.newCachedThreadPool();
+            executor.submit(r);
+            executor.shutdown();
         }
 
         JsonObj json = new JsonObj(resultSet.getRows());
